3e55a042cca1547dfa04ea5dee4d1e03f55583b7
Ndeye Awa Diop <nadiop97@gmail.com>
2021-01-28 13:20:09 +0100

    Agence creation seed set

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Agence.destroy_all 
+
+puts 'Creating Agence'
+25.times do
+  Agence.create(libelle: Faker::Company.industry , email: Faker::Internet.email, telephone: Faker::PhoneNumber.cell_phone_in_e164, adresse: Faker::Address.full_address, nb_voiture: Faker::Number.between(from: 1, to: 20))
+end
+
+puts "#{Agence.count} agences created !"
